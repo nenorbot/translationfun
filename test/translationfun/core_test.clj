d83f4efb981aebf920e0998ea2172ca3ca855297
ronen <ronen@ronen-laptop.home>
2013-10-11 20:17:24 +0200

    init

---
+++ test/translationfun/core_test.clj
@@ -0,0 +1,7 @@
+(ns translationfun.core-test
+  (:require [clojure.test :refer :all]
+            [translationfun.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
